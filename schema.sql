d168ef7bc98e727d458849dab9cfe268e2ee422c
SENIOR\Gian.Pasqualini <gian.pasqualini@senior.com.br>
2018-08-29 08:14:43 -0300

    exemplo da base

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA "ecossistema-reference_app";
+
+CREATE TABLE "ecossistema-reference_app".view_tarefa (
+    id CHARACTER VARYING(255),
+    titulo CHARACTER VARYING(255),
+    data DATE,
+    descricao CHARACTER VARYING(255),
+    ext CHARACTER VARYING(4000)
+);
+
---
+++ snapshot.sql
@@ -0,0 +1,3 @@
+-- Table: view_tarefa
+INSERT INTO "ecossistema-reference_app".view_tarefa(id,titulo,data,descricao,ext) VALUES("1234", 'comprar leite', '2018-08-24 18:20:17.0', 'leite integral', null);
+
